5b2427f79c00aceec884e6467d0c871335de8540
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-20 07:44:01 +0300

    Set up admin page

--- posts/models.py
+++ posts/models.py
@@ -3,13 +3,12 @@ from datetime import date
 from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
+from users.models import User
 
 # Create your models here.
 
-User = settings.AUTH_USER_MODEL
 
-
-class MourseQuerySet(models.QuerySet):
+class PostQuerySet(models.QuerySet):
 
     def search(self, query):
         lookup = (
@@ -22,9 +21,9 @@ class MourseQuerySet(models.QuerySet):
         return self.filter(lookup)
 
 
-class MourseManager(models.Manager):
+class PostManager(models.Manager):
     def get_queryset(self):
-        return MourseQuerySet(self.model, using=self._db)
+        return PostQuerySet(self.model, using=self._db)
 
     def search(self, query=None):
         if query is None:
@@ -32,30 +31,15 @@ class MourseManager(models.Manager):
         return self.get_queryset().search(query)
 
 
-class Mourse(models.Model):
+class Post(models.Model):
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(default='change-me', unique=True)
     content = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
-    start_date = models.DateField(default=date.today)
-    end_date = models.DateField(default="2021-12-25", null=True, blank=True)
-    q_lectures = models.PositiveIntegerField()
+    upload_date = models.DateField(default=date.today)
 
-    objects = MourseManager()
+    objects = PostManager()
 
     class Meta:
-        ordering = ['start_date']
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Mourse, self).save(*args, **kwargs)
-
-    def get_absolute_url(self):
-        return f"/my_mourse/{self.slug}"
-
-    def get_edit_url(self):
-        return f"{self.get_absolute_url()}/update"
-
-    def get_delete_url(self):
-        return f"{self.get_absolute_url()}/delete"
+        ordering = ['upload_date']
