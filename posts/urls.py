5eacaa946b23b2489fa94f388b89dcd392b7d6d1
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-21 07:37:10 +0300

    Add and connect post_list_view

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import post_detail_view
+from .views import post_detail_view, post_list_view
 
 urlpatterns = [
     path('<int:post_id>/', post_detail_view, name='post_detail'),
-
-    # path('post')
+    path('', post_list_view, name='post_list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,20 @@ from django.http import Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Post
+
+
 # Create your views here.
+
+
+def post_list_view(requests, *args, **kwargs):
+    query_set = Post.objects.all()
+    post_list = [{"id": post.id, "title": post.title, "slug": post.slug, "content": post.content,
+                  "upload_date": post.upload_date} for post in query_set]
+    data = {
+        "response": post_list
+    }
+
+    return JsonResponse(data)
 
 
 def post_detail_view(request, post_id, *args, **kwargs):
@@ -12,12 +25,10 @@ def post_detail_view(request, post_id, *args, **kwargs):
     status = 200
     try:
         obj = Post.objects.get(id=post_id)
-        data['content'] = obj.content,
         data['title'] = obj.title,
         data['slug'] = obj.slug,
         data['content'] = obj.content,
         data['upload_date'] = obj.upload_date,
-        data['image_path'] = obj.image.url,
     except:
         data['message'] = f"Oops... I have not found {post_id}"
         status = 200
