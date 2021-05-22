cde8055195ce5530623c42f6a34355cd504aa7c5
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-22 07:09:40 +0300

    Add create_user UserManager

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if username == None:
+            raise TypeError('User should have a username')
+
+        if email == None:
+            raise TypeError('User should have a email')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
