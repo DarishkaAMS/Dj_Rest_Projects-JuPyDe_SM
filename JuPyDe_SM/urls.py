3fbd62cfad871c325cc7396fc39c56c0409868d7
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-22 11:06:31 +0300

    Add Swager with drf_yasg

--- JuPyDe_SM/urls.py
+++ JuPyDe_SM/urls.py
@@ -15,12 +15,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from .views import home_page_view
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="JuPyDe_SM API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.ourapp.com/policies/terms/",
+        contact=openapi.Contact(email="contact@jupyde_sm.local"),
+        license=openapi.License(name="TEST License"),
+    ),
+    validators=['ssv', 'flex'],
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page_view, name='index'),
+    # path('', home_page_view, name='index'),
     path('posts/', include('posts.urls')),
     path('authentication/', include('authentication.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=None), name='schema-redoc'),
 ]
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+
 from .views import RegisterView, VerifyEmail
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
