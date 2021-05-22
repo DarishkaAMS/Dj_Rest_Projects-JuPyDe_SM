89caa768de30e260f30029be3b665c14d47376dd
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-22 09:09:21 +0300

    Add Email Sending

--- JuPyDe_SM/settings.py
+++ JuPyDe_SM/settings.py
@@ -129,3 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+
+
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
+from .models import User
 from .serializers import RegisterSerializer
+from .utils import Util
 # Create your views here.
 
 
@@ -16,4 +20,20 @@ class RegisterView(generics.GenericAPIView):
         serializer.save()
         user_data = serializer.data
 
+        user = User.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        absurl = 'http://' + current_site + relativeLink + "?token=" + str(token)
+        email_body = 'Hi ' + user.username + \
+                     ' Use the link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email,
+                'email_subject': 'Verify your email'}
+
+        Util.send_email(data)
         return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
