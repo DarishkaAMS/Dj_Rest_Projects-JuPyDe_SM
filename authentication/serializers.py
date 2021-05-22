beda88d72b157ebb1bd52d40ca9059f6f101897f
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-22 07:42:08 +0300

    Add RegisterSerializer

---
+++ authentication/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68, min_length=8, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+
+    def validate(self, attrs):
+        return super().validate(attrs)
