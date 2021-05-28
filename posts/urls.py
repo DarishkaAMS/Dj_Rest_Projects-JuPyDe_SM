fdc2639f826ff2169fbc2bf3e9941ef4c828e19e
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-05-28 14:53:45 +0300

    Update urls.py
    Update likes url

--- posts/urls.py
+++ posts/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', PostListAPIView.as_view(), name="posts"),
     path('<int:id>', PostDetailAPIView.as_view(), name="post"),
 #     path('posts_like/<int:id>', like_unlike_post, name='like-post-view'),
-    path('post/<int:post_pk>/<int:user_pk>/like/', PostLikeView.as_view(), name='post_like'),
-    path('post/<int:post_pk>/<int:user_pk>/dislike/', PostDislikeView.as_view(), name='post_dislike'),
+    path('posts_like/<int:post_pk>/<int:user_pk>/like/', PostLikeView.as_view(), name='post_like'),
+    path('posts_dislike/<int:post_pk>/<int:user_pk>/dislike/', PostDislikeView.as_view(), name='post_dislike'),
     path('posts_like/analitics/date_from=<date_from>&date_to=<date_to>/', PostAnaliticsLikesView.as_view(), name='post_likes'),
 
 ]
