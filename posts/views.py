7f7a511f7a61a3ee7563218729ec11c12231e134
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-05-28 12:10:16 +0300

    Update views.py
    Add PostLikeView and PostDislikeView

--- posts/views.py
+++ posts/views.py
@@ -32,28 +32,36 @@ class PostDetailAPIView(RetrieveUpdateDestroyAPIView):
         return self.queryset.filter(author=self.request.user)
 
 
-def like_unlike_post(request):
-    user = request.user
-    if request.method == 'POST':
-        post_id = request.POST.get('post_id')
-        post_obj = Post.objects.get(id=post_id)
-        user = User.objects.get(user=user)
+# def like_unlike_post(request):
+#     user = request.user
+#     if request.method == 'POST':
+#         post_id = request.POST.get('post_id')
+#         post_obj = Post.objects.get(id=post_id)
+#         user = User.objects.get(user=user)
 
-        if user in post_obj.liked.all():
-            post_obj.liked.remove(user)
-        else:
-            post_obj.liked.add(user)
+#         if user in post_obj.liked.all():
+#             post_obj.liked.remove(user)
+#         else:
+#             post_obj.liked.add(user)
 
-        like, created = PostLike.objects.get_or_create(user=user, post_id=post_id)
+#         like, created = PostLike.objects.get_or_create(user=user, post_id=post_id)
 
-        if not created:
-            if like.value == 'Like':
-                like.value = 'Unlike'
-            else:
-                like.value = 'Like'
-        else:
-            like.value = 'Like'
+#         if not created:
+#             if like.value == 'Like':
+#                 like.value = 'Unlike'
+#             else:
+#                 like.value = 'Like'
+#         else:
+#             like.value = 'Like'
 
-            post_obj.save()
-            like.save()
+#             post_obj.save()
+#             like.save()
 
+class PostLikeView(generics.CreateAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+
+class PostDislikeView(generics.CreateAPIView):
+    queryset = Dislike.objects.all()
+    serializer_class = DislikeSerializer
