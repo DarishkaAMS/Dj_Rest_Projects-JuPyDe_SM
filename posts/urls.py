e634819948fe8fd8d441714d699b0cae1b015756
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-05-28 14:17:44 +0300

    Update urls.py
    Add PostAnaliticsLikes/Dislike url

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import PostDetailAPIView, PostListAPIView, like_unlike_post
+from .views import PostDetailAPIView, PostListAPIView, like_unlike_post, PostAnaliticsLikesView, PostDislikeView, PostLikeView
 
 
 urlpatterns = [
     path('', PostListAPIView.as_view(), name="posts"),
     path('<int:id>', PostDetailAPIView.as_view(), name="post"),
-    path('posts_like/<int:id>', like_unlike_post, name='like-post-view'),
+#     path('posts_like/<int:id>', like_unlike_post, name='like-post-view'),
+    path('post/<int:post_pk>/<int:user_pk>/like/', PostLikeView.as_view(), name='post_like'),
+    path('post/<int:post_pk>/<int:user_pk>/dislike/', PostDislikeView.as_view(), name='post_dislike'),
+    path('posts_like/analitics/date_from=<date_from>&date_to=<date_to>/', PostAnaliticsLikesView.as_view(), name='post_likes'),
 
 ]
