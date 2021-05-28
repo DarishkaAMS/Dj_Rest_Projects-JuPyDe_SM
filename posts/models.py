b59402fbbd8c4056bd08f0c06e29eebb9913db6f
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-05-28 11:57:07 +0300

    Update models.py
    Add PostLike and PostDislike classes

--- posts/models.py
+++ posts/models.py
@@ -19,16 +19,34 @@ class Post(models.Model):
         return str(self.title)
 
 
-LIKE_CHOICES = (
-    ('Like', 'Like'),
-    ('Unlike', 'Unlike'),
-)
+# LIKE_CHOICES = (
+#     ('Like', 'Like'),
+#     ('Unlike', 'Unlike'),
+# )
+
+
+# class PostLike(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     value = models.CharField(choices=LIKE_CHOICES, max_length=8)
+
+#     def __str__(self):
+#         return f"{self.user}-{self.post}-{self.value}"
 
 
 class PostLike(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    value = models.CharField(choices=LIKE_CHOICES, max_length=8)
+    like_user = models.ForeignKey(StarnaviUser, related_name='like_user', on_delete=models.CASCADE)
+    like_post = models.ForeignKey(Post, related_name='like_post', on_delete=models.CASCADE)
+    like = models.SmallIntegerField(default=0)
+    like_published = models.DateField(format('%Y-%m-%d'), auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.user}-{self.post}-{self.value}"
+
+class PostDislike(models.Model):
+    dislike_user = models.ForeignKey(StarnaviUser, related_name='dislike_user', on_delete=models.CASCADE)
+    dislike_post = models.ForeignKey(Post, related_name='dislike_post', on_delete=models.CASCADE)
+    dislike = models.SmallIntegerField(default=0)
+    dislike_published = models.DateField(format('%Y-%m-%d'), auto_now_add=True)
+
+
+
+
